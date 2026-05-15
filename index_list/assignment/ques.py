85fce81934587b6cb1653bbf83e3c3ce911496b6
Nikita <nikita2024nikki@gmail.com>
2026-05-15 15:59:45 +0530

    class questions

---
+++ index_list/assignment/ques.py
@@ -0,0 +1,13 @@
+
+"None"
+print(None == None)
+
+"Docstring"
+def add(a, b):
+    """This function adds two numbers"""
+   
+    return a + b
+
+print(add(5, 3))
+print(add.__doc__) 
+"print the docstring"
