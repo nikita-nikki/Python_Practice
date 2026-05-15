e8a560fdf7605c3fd265edddac8b53119c7acef0
Nikita <nikita2024nikki@gmail.com>
2026-05-15 15:32:28 +0530

    list

--- index_list/hands_on/list.py
+++ index_list/hands_on/list.py
@@ -1,17 +1,28 @@
 digits = [0,1,2,3,4,5,6,7,]
 
+"index"
 print(digits[0])  
 print(digits[1]) 
 
+"append"
 digits.append(9)
 print(digits)
 
+"count"
 count = digits.count(6)
 print(count)
 
+"type keyword"
 print(type(count))
 print(type(digits))
 
+"insert"
 digits.insert(3, 10)
 "inser(index, value)"
 print(digits)
+
+"copy"
+new_list = digits.copy()
+print(new_list)
+
+
