730e489b07fea4421586093dc8dba69432a60331
Nikita <nikita2024nikki@gmail.com>
2026-05-21 15:48:29 +0530

    Operator practice

---
+++ 6_operator/hands_on/types_of_operator.py
@@ -0,0 +1,46 @@
+"Operators are used to perform operations on variables and values."
+
+# Python divides the operators in the following groups:
+
+# 1.Arithmetic operators
+# 2.Assignment operators
+# 3.Comparison operators
+# 4.Logical operators
+# 5.Identity operators
+# 6.Membership operators
+# 7.Bitwise operators
+
+# 1. Arithmetic operators
+a = 2
+b = 3
+c = a + b
+print("Use of Arithmetic operators:",c)
+
+# 4. Logical operators
+print("and")
+x = 0
+y = 0
+z = x and y
+print("Use of and Logical operators:",z)
+
+a = 0
+b = 1
+c = a and b
+print("Use of and Logical operators:",c)
+
+
+print("or")
+x = 0
+y = 0
+z = x or y      
+print("Use of or Logical operators:",z)
+
+a = 0
+b = 1       
+c = a or b
+print("Use of or Logical operators:",c)
+
+print("not")
+x = 0
+y = not x
+print("Use of not Logical operators:",y)
