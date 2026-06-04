3cb19cba78bb193f50d53ec146b593c138f62701
Nikita <nikita2024nikki@gmail.com>
2026-06-04 11:55:55 +0530

    built-in-function practice

--- 13_built_in_function/hands_on/practice.py
+++ 13_built_in_function/hands_on/practice.py
@@ -1,35 +1,61 @@
-# len() – Length of an object
-fruits = ["apple", "banana", "mango"]
-print(len(fruits))
-
-# type() – Check data type
-x = 10
-y = "Hello"
-
-print(type(x))
-print(type(y))
-
-# int(), float(), str()
-a = "100"
-
-print(int(a))
-print(float(a))
-print(str(123))  
-# Used for type conversion.
-
-# abs() – Absolute value
-print(abs(-15))
-print(abs(15))
 
 
-# max() and min()
-nums = [10, 20, 5, 30]
 
-print(max(nums))
-print(min(nums))
+# type() → Returns the data type of a variable
+print("type: ",type(10))
+
+# len() → Returns the number of items in a sequence
+print("len: ",len("Python"))
+
+# int() → Converts value into integer
+print("int: ", int("100"))
+
+# float() → Converts value into decimal number
+print("float: ",float("3.14"))
+
+# str() → Converts value into string
+print("str: ",str(123))
+
+# sum() → Adds all numbers in an iterable
+print("sum: ",sum([10, 20, 30]))
+
+# max() → Returns the largest value
+print("max: ",max([5, 2, 9]))
+
+# min() → Returns the smallest value
+print("min: ",min([5, 2, 9]))
+
+# sorted() → Returns a sorted list
+print("sorted: ",sorted([3, 1, 2]))
+print("tyoe of sorted: ",type(sorted("pythoN")))
+
+# range() → Generates a sequence of numbers
+print("range: ",(range(5)))
 
 
-# sum()
-nums = [1, 2, 3, 4]
+# enumerate() → Returns index and value pairs
+print("enumerate")
+for i, v in enumerate(["A", "B", "C"]):
+    print(i, v)
 
-print(sum(nums))
+# abs() → Returns absolute (positive) value
+print("abs: ",abs(-10))
+
+# round() → Rounds a number to given decimal places
+print(round(3.14159, 2))
+
+# any() → True if at least one value is True
+print(any([0, 1, 0]))
+
+# all() → True if all values are True
+print(all([1, 1, 1]))
+
+# isinstance() → Checks data type of a variable
+print("isinstance: ", isinstance(10, int))
+
+# id() → Returns memory address of object
+print(id("Python"))
+
+# pow() → Returns power of a number
+print(pow(2, 3))
+
